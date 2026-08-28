b155dbbc3c2cfc70068fc31871c8e90161b65033
grubiy2004 <stebeltsof@gmail.com>
2026-08-28 16:08:46 +0300

    asm instead of TaskDelay()

--- source/rtos.c
+++ source/rtos.c
@@ -20,12 +20,15 @@ static void vLED_Task(void *pvParameters) {
         GPIOC->BRR = GPIO_BRR_BR13;
 
         // Задержка 500 мс (используем системный тик FreeRTOS)
-        vTaskDelay(pdMS_TO_TICKS(100));
+        //vTaskDelay(pdMS_TO_TICKS(100));
+        for(int i=0;i<10000000;i++) {
+            __ASM volatile("nop");
+        }
 
         // Выключить светодиод (высокий уровень на PC13)
         GPIOC->BSRR = GPIO_BSRR_BS13;
 
-        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
