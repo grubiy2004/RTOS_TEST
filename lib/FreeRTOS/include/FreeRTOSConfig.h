39fbe110a28c3a3b43d18dffbf1a946042a58f30
grubiy2005 <stebeltsoff.kolya@yandex.ru>
2026-09-09 23:48:49 +0300

    Enable Idle task

--- lib/FreeRTOS/include/FreeRTOSConfig.h
+++ lib/FreeRTOS/include/FreeRTOSConfig.h
@@ -18,7 +18,7 @@
  *----------------------------------------------------------*/
 
 #define configUSE_PREEMPTION                    1
-#define configUSE_IDLE_HOOK                     0
+#define configUSE_IDLE_HOOK                     1
 #define configUSE_TICK_HOOK                     0
 #define configCPU_CLOCK_HZ                      ( 72000000UL )
 #define configTICK_RATE_HZ                      ( 1000UL )
