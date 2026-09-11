c248dba7308543662cce3020f7676e0b2a3d1415
grubiy2004 <stebeltsof@gmail.com>
2026-09-11 11:58:54 +0300

    Added doxygen comments

--- source/ssd1306.c
+++ source/ssd1306.c
@@ -4,7 +4,9 @@
 // Внутренний буфер кадра (1024 байта = 128 * 64 / 8)
 static uint8_t ssd1306_buffer[SSD1306_WIDTH * SSD1306_HEIGHT / 8];
 
-// Статическая функция для отправки пакета "команда"
+/** @brief Transmit command
+ * @note static
+*/
 static void SSD1306_WriteCmd(uint8_t cmd) {
     I2C_Start();
     I2C_Select(SSD1306_ADDR, 0); // Запись
@@ -13,7 +15,9 @@ static void SSD1306_WriteCmd(uint8_t cmd) {
     I2C_Stop();
 }
 
-// Статическая функция для отправки пакета "данные"
+/** @brief Transmit data
+ * @note static
+ */
 static void SSD1306_WriteData(uint8_t *data, uint16_t len) {
     I2C_Start();
     I2C_Select(SSD1306_ADDR, 0); // Запись
@@ -24,7 +28,7 @@ static void SSD1306_WriteData(uint8_t *data, uint16_t len) {
     I2C_Stop();
 }
 
-// Инициализация дисплея
+/** @brief Initialization of display */
 void SSD1306_Init(void) {
     // Ждем стабилизации питания (в твоем стиле, delay уже реализован)
     delay(10000);
@@ -33,25 +37,25 @@ void SSD1306_Init(void) {
 	I2C_Select(SSD1306_ADDR, 0); // Запись
 	I2C_Write(0x00);             // Control Byte: Co=0, D/C#=0 (Command)
 	I2C_Write(SSD1306_CMD_DISPLAY_OFF);
-	
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_SET_MUX_RATIO);
 	I2C_Write(0x3F);
-	
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_SET_DISP_OFFSET);
 	I2C_Write(0x00);
-    
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_SET_START_LINE | 0x00);
 	I2C_Write(0x00);
-	
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_SEG_REMAP_1);
-	
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_COM_SCAN_DEC);
-	
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_SET_COM_PINS);
 	I2C_Write(0x12);
@@ -59,38 +63,37 @@ void SSD1306_Init(void) {
     	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_SET_CONTRAST);
 	I2C_Write(0xFF);
-	
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_NORMAL_DISPLAY);
-	
+
     	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_SET_CLK_DIV);
 	I2C_Write(0x80);
-	
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_CHARGE_PUMP);
 	I2C_Write(0x14);
-	
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_SET_MEM_MODE);
 	I2C_Write(0x00);
-	
+
 	SSD1306_Clear();
-	
+
 	I2C_Start();
 	I2C_Select(SSD1306_ADDR, 0); // Запись
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_DISPLAY_ON);
-	
+
 	I2C_Stop();
 }
-
-// Очистка буфера и экрана
+/** @brief Clear buffer and display */
 void SSD1306_Clear(void) {
     for(uint16_t i = 0; i < sizeof(ssd1306_buffer); i++) {
         ssd1306_buffer[i] = 0x00;
     }
-    
+
     // Устанавливаем окно на весь экран
 	I2C_Start();
 	I2C_Select(SSD1306_ADDR, 0); // Запись
@@ -103,12 +106,14 @@ void SSD1306_Clear(void) {
 	I2C_Write(SSD1306_CMD_SET_PAGE_ADDR);
 	I2C_Write(0x00);
 	I2C_Write(0x07);
-    
+
     // Отправляем пустые данные
     SSD1306_WriteData(ssd1306_buffer, sizeof(ssd1306_buffer));
 }
 
-// Полное обновление экрана из буфера
+/** @brief Full display updating from buffer
+ * @param buffer Array of pixels
+*/
 void SSD1306_UpdateFull(uint8_t *buffer) {
     // Копируем во внутренний буфер
     for(uint16_t i = 0; i < sizeof(ssd1306_buffer); i++) {
@@ -116,27 +121,27 @@ void SSD1306_UpdateFull(uint8_t *buffer) {
     }
     	I2C_Start();
 	I2C_Select(SSD1306_ADDR, 0); // Запись
-	
+
 	I2C_Write(0x00);             // Control Byte: Co=0, D/C#=0 (Command)
 	I2C_Write(SSD1306_CMD_SET_COL_ADDR);
 	I2C_Write(0x00);
 	I2C_Write(0x7F);
-	
+
 	I2C_Write(0x00);
 	I2C_Write(SSD1306_CMD_SET_PAGE_ADDR);
 	I2C_Write(0x00);
 	I2C_Write(0x07);
-    
+
     // Отправляем данные
     SSD1306_WriteData(ssd1306_buffer, sizeof(ssd1306_buffer));
 }
 
-// Включение дисплея
+/** @brief Enable display */
 void SSD1306_DisplayOn(void) {
     SSD1306_WriteCmd(SSD1306_CMD_DISPLAY_ON);
 }
 
-// Выключение дисплея
+/** @brief Disable display */
 void SSD1306_DisplayOff(void) {
     SSD1306_WriteCmd(SSD1306_CMD_DISPLAY_OFF);
 }
